85dcf2892204a21f8ec03efb4aa80529cfd65749
not1214 <91748221+not1214@users.noreply.github.com>
2022-04-04 00:07:46 +0900

    [Update]confirm.php

---
+++ PHP応用_演習/Views/confirm.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <?php include('head.php') ?>
+</head>
+
+<body>
+  <div class='main'>
+    <div class='container-fluid'>
+      <?php include("header.php") ?>
+
+      <div class='row justify-content-center mt-3'>
+        <h3>お問い合わせ内容確認</h3>
+      </div>
+
+      <div class='row'>
+        <form action='complete.php' method='post' class='row'>
+
+          <p class='col-6 offset-3 mt-2'>氏名</p>
+          <p class='col-6 offset-3'><?php $_POST['name'] ?></p>
+
+          <p class='col-6 offset-3 mt-2'>フリガナ</p>
+          <p class='col-6 offset-3'><?php $_POST['kana'] ?></p>
+
+          <p class='col-6 offset-3 mt-2'>電話番号</p>
+          <p class='col-6 offset-3'><?php $_POST['tel'] ?></p>
+
+
+          <p class='col-6 offset-3 mt-2'>メールアドレス</p>
+          <p class='col-6 offset-3'><?php $_POST['email'] ?></p>
+
+          <p class='col-6 offset-3 mt-2'>お問い合わせ内容</p>
+          <p class='col-6 offset-3'><?php $_POST['body'] ?></p>
+
+          <div class='col-12 my-3'>
+            <input type='button' value='キャンセル' class='button col-2 offset-4 btn-danger'>
+            <input type='submit' value='送信する' class='button col-2 btn-success ml-3'>
+          </div>
+        
+        </form>
+      </div>
+
+      <?php include('footer.php') ?>
+    </div>
+  </div>
+</body>
+
+</html>
--- PHP応用_演習/Views/contact.php
+++ PHP応用_演習/Views/contact.php
@@ -9,35 +9,34 @@
   <div class='main'>
     <div class='container-fluid'>
       <?php include("header.php") ?>
-      
+
+      <div class='row justify-content-center mt-3'>
+        <h3>お問い合わせフォーム</h3>
+      </div>
+
       <div class='row'>
-        <form action='confirm.php' method='post'>
-          <div>
-            <label for='name'>氏名</label>
-            <input id='name' type='text' name='name'>
-          </div>
-          <div>
-            <label for='kana'>フリガナ</label>
-            <input id='kana' type='text' name='kana'>
-          </div>
-          <div>
-            <label for='tel'>電話番号</label>
-            <input id='tel' type='number' name='tel'>
-          </div>
-          <div>
-            <label for='email'>メールアドレス</label>
-            <input id='email' type='email' name='email'>
-          </div>
-          <div>
-            <label for='body'>お問い合わせ内容</label>
-            <textarea id='body' name='body' placeholder='ご自由に質問を書いてください'></textarea>
-          </div>
-          <div>
-            <input type='submit' value='送信する'>
-          </div>
+        <form action='confirm.php' method='post' class='row'>
+
+          <label for='name' class='col-6 offset-3 mt-2'>氏名</label>
+          <input id='name' type='text' name='name' class='col-6 offset-3 form-control'>
+
+          <label for='kana' class='col-6 offset-3 mt-2'>フリガナ</label>
+          <input id='kana' type='text' name='kana' class='col-6 offset-3 form-control'>
+
+          <label for='tel' class='col-6 offset-3 mt-2'>電話番号</label>
+          <input id='tel' type='number' name='tel' class='col-6 offset-3 form-control'>
+
+          <label for='email' class='col-6 offset-3 mt-2'>メールアドレス</label>
+          <input id='email' type='email' name='email' class='col-6 offset-3 form-control'>
+
+          <label for='body' class='col-6 offset-3 mt-2'>お問い合わせ内容</label>
+          <textarea id='body' name='body' placeholder='ご自由に質問を書いてください' class='col-6 offset-3 form-control' rows='5'></textarea>
+
+          <input type='submit' value='確認する' class='button col-2 offset-5 my-3 btn-success'>
+
         </form>
       </div>
-      
+
       <?php include('footer.php') ?>
     </div>
   </div>

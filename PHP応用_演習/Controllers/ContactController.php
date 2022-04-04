d47aa3a8e169292ac48af1eac488386a35290311
not1214 <91748221+not1214@users.noreply.github.com>
2022-04-04 18:09:54 +0900

    [Update]Controller(index)

---
+++ PHP応用_演習/Controllers/ContactController.php
@@ -0,0 +1,31 @@
+<?php
+require_once('../Models/Contact.php');
+
+class ContactController {
+    private $request;  //リクエストパラメータ(GET,POST)
+    private $Contact;  //Contactモデル
+
+    public function __construct() {
+        //リクエストパラメータの取得
+        $this->request['get'] = $_GET;
+        $this->request['post'] = $_POST;
+        //モデルオブジェクトの作成
+        $this->Contact = new Contact();
+        //別モデルと連携
+        $dbh = $this->Contact->get_db_handler();
+    }
+
+    public function index() {
+      $page = 0;
+      if (isset($this->request['get']['page'])) {
+          $page = $this->request['get']['page'];
+      }
+
+      $contacts = $this->Contact->findAll($page);
+      $contacts_count = $this->Contact->countAll();
+      $params = ['contacts' => $contacts, 'pages' => $contacts_count / 20, 'page' => $page];
+      return $params;
+    }
+}
+
+?>
